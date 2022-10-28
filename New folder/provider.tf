a34baadfc4aa36f29f618e63b40bf7ddc9c53eed
subbu <venkysubby97@gmail.com>
2022-10-28 14:01:08 +0000

    folders

---
+++ New folder/provider.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+
+  version = "=3.0.0"
+  
+}
+
+terraform {
+  backend "azurerm" {
+    storage_account_name = "automationpurpose"
+    container_name       = "logs1"
+    key                  = "prod.terraform.tfstate"
+    access_key = "C2QxvyaObIQh3oyUEgX5RxbYGTxFojGZnQ/iz7BwMYiWj/QMj3qGXDcYlOt0B8gd6oss8jl4WMn8+AStDNYDvA=="
+  }
+}
+           
