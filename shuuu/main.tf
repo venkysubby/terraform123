965bb39723f93a24cfe7f5d5a3d557866d213a8a
subbu <venkysubby97@gmail.com>
2022-10-28 15:11:15 +0000

    holders

---
+++ shuuu/main.tf
@@ -0,0 +1,39 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "imagerg23"
+  location = "East us"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "acrproject32676"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aksproject2326"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "aksaks1"
+
+  default_node_pool {
+    name       = "system"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+resource "azurerm_role_assignment" "acrpull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
---
+++ shuuu/provider.tf
@@ -0,0 +1,16 @@
+provider "azurerm" {
+  features {}
+
+  version = "=3.0.0"
+  
+}
+
+terraform {
+  backend "azurerm" {
+    storage_account_name = "strogaer343"
+    container_name       = "logs"
+    key                  = "prod.terraform.tfstate"
+    access_key = "ulKMQ35d7YipEVIwONoOYRv3d4DyG8kHuf1E5tMa4YOqnq4W1I7sgWJ623DZT22ClnEaTqd3e4Yu+AStNlQDzw=="
+  }
+}
+           
