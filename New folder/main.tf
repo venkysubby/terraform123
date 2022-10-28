66048386a3988490bd817df41d2ca5a14ea11a2b
venkysubby <91669076+venkysubby@users.noreply.github.com>
2022-10-28 14:23:42 +0000

    Update main.tf

--- New folder/main.tf
+++ New folder/main.tf
@@ -20,7 +20,6 @@ resource "azurerm_container_registry" "acr" {
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
-  kubernetes_version  = var.kubernetes_version
   location            = var.location
   resource_group_name = azurerm_resource_group.aks-rg.name
   dns_prefix          = var.cluster_name
