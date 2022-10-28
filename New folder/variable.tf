72fefb71757611df71e19d7221aeadf3a95642d7
venkysubby <91669076+venkysubby@users.noreply.github.com>
2022-10-28 14:23:16 +0000

    Update variable.tf

--- New folder/variable.tf
+++ New folder/variable.tf
@@ -10,10 +10,6 @@ variable "cluster_name" {
   type        = string
   description = "AKS name in Azure"
 }
-variable "kubernetes_version" {
-  type        = string
-  description = "Kubernetes version"
-}
 variable "system_node_count" {
   type        = number
   description = "Number of AKS worker nodes"
